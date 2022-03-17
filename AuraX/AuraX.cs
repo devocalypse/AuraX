85660e23b0ce146beb5444cd55f8ecefe5b88546
Devocalypse <Devocalypse@AEON>
2022-03-17 18:13:09 +0200

    Add project files.

---
+++ AuraX/AuraX.cs
@@ -0,0 +1,75 @@
+﻿using System.ServiceProcess;
+using AuraServiceLib;
+
+namespace AuraX
+{
+    public partial class AuraX : ServiceBase
+    {
+        private readonly IAuraSdk2 sdk;
+        private IAuraSyncDeviceCollection syncDevices;
+        public AuraX()
+        {
+            //Allow actions on workstation session changes
+            CanHandleSessionChangeEvent = true;
+
+            InitializeComponent();
+            sdk = (IAuraSdk2)new AuraSdk();
+            syncDevices = sdk.Enumerate(0);
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            //Handle session changes via https://tousu.in/?qa=917098/
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLogon:
+                    //Debug.WriteLine(changeDescription.SessionId + " logon");
+                    break;
+                case SessionChangeReason.SessionLogoff:
+                    //Debug.WriteLine(changeDescription.SessionId + " logoff");
+                    break;
+                case SessionChangeReason.SessionLock:
+                    //Session is locked
+                    Darken();
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    //Session unlocked - release
+                    Release();
+                    break;
+            }
+
+            base.OnSessionChange(changeDescription);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+            Release();
+        }
+
+        private void Darken()
+        {
+            sdk.SwitchMode();
+            
+            foreach (IAuraSyncDevice dev in syncDevices)
+            {
+                foreach (IAuraRgbLight light in dev.Lights)
+                {
+                    //Console.WriteLine($"R={light.Red} G={light.Green} B={light.Blue}");
+                    light.Color = 0x00000000;
+                }
+
+                dev.Apply();
+            }
+        }
+
+        private void Release()
+        {
+            sdk.ReleaseControl(0);
+        }
+    }
+}
---
+++ AuraX/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Reflection;
+
+namespace AuraX
+{
+    class Program : ServiceBase
+    {
+        static void Main(string[] args)
+        {
+            if (System.Environment.UserInteractive)
+            {
+                switch (string.Concat(args))
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                        StartService();
+                        break;
+                    case "--uninstall":
+                        StopService();
+                        ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--stop":
+                        StopService();
+                        break;
+                    case "--start":
+                        StartService();
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase.Run(new AuraX());
+            }
+        }
+
+        public static void StopService()
+        {
+            using (ServiceController service = new ServiceController("AuraX"))
+            {
+                if (service.Status != ServiceControllerStatus.Stopped && service.Status != ServiceControllerStatus.StopPending)
+                    service.Stop();
+            }
+        }
+
+        public static void StartService()
+        {
+            using (ServiceController service = new ServiceController("AuraX"))
+            {
+                if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StopPending)
+                    service.Start();
+            }
+            
+        }
+
+    }
+}
